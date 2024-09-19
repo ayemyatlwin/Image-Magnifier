98d1d9ce8b34cb61a83f3bad5f8c7145f94471c1
AyeMyatLwin <108206521+ayemyatlwin@users.noreply.github.com>
2024-09-19 17:12:25 +0630

    [Update] showing in home page , update readme file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import ImageMagnifier from "./components/ImageMagnifier";
 
 export default function Home() {
   return (
     <div>
       <h1>Image Magnifier in Next.js 14</h1>
-     
+      <ImageMagnifier
+        src="/images/hat2.png"
+        alt="Example Image"
+        zoomLevel={2}
+      />
     </div>
   );
 }
